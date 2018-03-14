1f021ff583c4a947f30b7b65bbb80a76a8b525d8
GBTC441020ur <soficebotari>
2018-03-14 15:27:07 -0400

    spring-boot-devtools<artifactId> added

---
+++ src/main/java/com/example/demo/RoleRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+import sun.util.resources.ru.CalendarData_ru;
+
+public interface RoleRepository extends CrudRepository<Role, Long>{}
